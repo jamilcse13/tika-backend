b2fa9a029e516f497807395142de8c4ce793cd49
jamil <jamil.cse13@gmail.com>
2022-02-02 01:33:16 +0600

    created api which hitted from frontend

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('test', function () {
+    return 'Changes this status';
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
