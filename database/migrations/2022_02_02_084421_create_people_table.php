cbf6b914819a2238ea3f0c5c59d2126402ae78d9
jamil <jamil.cse13@gmail.com>
2022-02-02 15:20:43 +0600

    created person and category model, db, controller

---
+++ database/migrations/2022_02_02_084421_create_people_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_no');
+            $table->dateTime('dob');
+            $table->string('office');
+            $table->unsignedBigInteger('registered')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
